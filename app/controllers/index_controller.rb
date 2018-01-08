f901eec2a8af6c49a4e062e441d5b7cffd89837b
lebrecht <mlebrecht@operantmedia.com>
2018-01-07 17:54:45 -0800

    add pages

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,9 @@
+class IndexController < ApplicationController
+
+  def home
+  end
+
+  def about
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: => 'index#home'
+  get 'index/home', to: 'index#home'
+  get 'index/about', to: 'index#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
